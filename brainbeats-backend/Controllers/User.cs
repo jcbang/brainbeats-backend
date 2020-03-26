1fd7a8971518820e47bb84ed7a5e61d68a58bf3c
Restyled.io <commits@restyled.io>
2020-03-26 05:53:16 +0000

    Restyled by dotnet-format

--- brainbeats-backend/Controllers/User.cs
+++ brainbeats-backend/Controllers/User.cs
@@ -1,28 +1,36 @@
-﻿namespace brainbeats_backend.Controllers {
-  public class User {
-    public string id {
-      get;
-      set;
+﻿namespace brainbeats_backend.Controllers
+{
+    public class User
+    {
+        public string id
+        {
+            get;
+            set;
+        }
+        public string email
+        {
+            get;
+            set;
+        }
+        public string firstName
+        {
+            get;
+            set;
+        }
+        public string lastName
+        {
+            get;
+            set;
+        }
+        public string[] savedBeats
+        {
+            get;
+            set;
+        }
+        public string[] savedPlaylists
+        {
+            get;
+            set;
+        }
     }
-    public string email {
-      get;
-      set;
-    }
-    public string firstName {
-      get;
-      set;
-    }
-    public string lastName {
-      get;
-      set;
-    }
-    public string[] savedBeats {
-      get;
-      set;
-    }
-    public string[] savedPlaylists {
-      get;
-      set;
-    }
-  }
 }
--- brainbeats-backend/Startup.cs
+++ brainbeats-backend/Startup.cs
@@ -11,40 +11,47 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace brainbeats_backend {
-public class Startup {
-  public Startup(IConfiguration configuration) {
-    Configuration = configuration;
-  }
+namespace brainbeats_backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-  public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-  // This method gets called by the runtime. Use this method to add services to
-  // the container.
-  public void ConfigureServices(IServiceCollection services) {
-    services.AddControllers();
-    services.AddCors(options => {
-      options.AddPolicy("Allow-All", builder => { builder.AllowAnyOrigin(); });
-    });
-  }
+        // This method gets called by the runtime. Use this method to add services to
+        // the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Allow-All", builder => { builder.AllowAnyOrigin(); });
+            });
+        }
 
-  // This method gets called by the runtime. Use this method to configure the
-  // HTTP request pipeline.
-  public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-    if (env.IsDevelopment()) {
-      app.UseDeveloperExceptionPage();
+        // This method gets called by the runtime. Use this method to configure the
+        // HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(
+                endpoints => { endpoints.MapControllers().RequireCors("Allow-All"); });
+        }
     }
-
-    app.UseHttpsRedirection();
-
-    app.UseRouting();
-
-    app.UseCors();
-
-    app.UseAuthorization();
-
-    app.UseEndpoints(
-        endpoints => { endpoints.MapControllers().RequireCors("Allow-All"); });
-  }
-}
 }
